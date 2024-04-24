07498a860a49b99906b037c0e96d6af5e606a4cf
Jiang2905 <kangsadiputra29@gmail.com>
2024-04-24 19:59:47 +0700

    commit 1

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const path =require('path')
+const app = express(); 
+const port = 6969;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+    res.render('login'); // Assuming Login.ejs exists in views directory
+  });
+app.get('/', (req, res) => {
+    res.render('index'); // Assuming Login.ejs exists in views directory
+  });
+  app.get('/signup', (req, res) => {
+    res.render('signup'); // Assuming Login.ejs exists in views directory
+  });
+
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);
+  });
